56e955d4978716b15f4a2b3c04dfb356db5634e8
KFalcao <karollinefalcao@gmail.com>
2025-03-31 15:21:05 -0300

    ":zap: Feat: add conexão com api do back"

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,34 @@
+import S from './doados.module.scss'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+
 export default function Doados(){
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        const response = await axios.get("https://desafio-api-wae0.onrender.com/livros")
+        setLivros(response.data)
+    }
+
+    useEffect(()=>{
+        getLivros()
+    }, [livros])
+
     return(
-        <section>
+        <section className={S.boxDoados}>
             <h2>Livros Doados</h2>
+            <section className={S.boxBooks}>
+                {livros.map((item)=>(
+                    <article key={item.id}>
+                        <img src={item.imagem_url} alt="capa do livro" />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.categoria}</p>
+                        <p>{item.autor}</p>
+                    </article>
+                ))}
+            </section>
         </section>
     )
 }
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,22 +1,69 @@
 import S from './queroDoar.module.scss'
 import livroIcon from '../../assets/book-icon.png'
 
+import axios from 'axios'
+import { useState } from 'react'
+
 export default function QueroDoar(){
+
+
+    const[titulo, setTitulo] = useState ("")
+    const[categoria, setCategoria] = useState ("")
+    const[autor, setAutor] = useState ("")
+    const[imagem_url, setImagem_url] = useState ("")
+
+    const enviarDados = async() =>{
+
+        const urlApi = "https://desafio-api-wae0.onrender.com/doar"
+        
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url,
+        }
+
+        await axios.post(urlApi, dadosEnviar)
+
+        alert ("Livro cadastrado!")
+
+        setTitulo("")
+        setCategoria("")
+        setAutor("")
+        setImagem_url("")
+    }
+
+    const capturaTitulo = (e) =>{
+        setTitulo(e.target.value)
+        console.log(e.target.value)
+    }
+
+    const capturaCategoria = (e) =>{
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) =>{
+        setAutor(e.target.value)
+    }
+
+    const capturaImagem = (e) =>{
+        setImagem_url(e.target.value)
+    }
+
     return(
         <section className={S.principal}>
             <section className={S.informacoes}>
             <h2>Por favor, preencha o formulário com suas informações e as informações do Livro.</h2>
-            <form action=""className={S.boxFormulario}>
+            <form onSubmit={(e) => e.defaultPrevented()}className={S.boxFormulario}>
                 <div>
                     <img src={livroIcon} alt="Icone de livro aberto" />
                     <h3>Informações do Livro</h3>
                 </div>
-                <input type="text" placeholder='Título'/>
+                <input type="text" placeholder='Título' onChange={capturaTitulo} value={titulo}/>
                 <input type="text" placeholder='Categoria'/>
                 <input type="text" placeholder='Autor'/>
                 <input type="text" placeholder='Link da Imagem'/>
-
-                <button>Doar</button>            
+                <button onClick={enviarDados}>Doar</button>            
             </form>
             </section>
         </section>
